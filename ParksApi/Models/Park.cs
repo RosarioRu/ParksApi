08e856c06975515dc997933b9eb1554ee6ab7e97
RosarioRu <mariarruval@gmail.com>
2022-09-09 12:09:45 -0700

    Add required and string length limit for Park(s) properties.

--- ParksApi/Models/Park.cs
+++ ParksApi/Models/Park.cs
@@ -5,10 +5,21 @@ namespace ParksApi.Models
     public class Park
     {
         public int ParkId{ get; init; }
+
+        [Required]
         public string ParkName { get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [Required]
+        [StringLength(12)]
         public string State { get; set; }
+
+        [Required]
         public DateTime DateEstablished { get; init; }
+
+        [Required]
         public int AreaInAcres { get; set; }
     } 
 }
