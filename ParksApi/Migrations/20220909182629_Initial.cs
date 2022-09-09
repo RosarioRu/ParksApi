da6bcdb47aaddd6ee94a7fba69fd91596b574324
RosarioRu <mariarruval@gmail.com>
2022-09-09 11:28:41 -0700

    Complete initial migration.

---
+++ ParksApi/Migrations/20220909182629_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ParkName = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    City = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    State = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    DateEstablished = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    AreaInAcres = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
