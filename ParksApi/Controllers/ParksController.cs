0524621182834547a50500ae8ee0b65b3440844a
RosarioRu <mariarruval@gmail.com>
2022-09-09 12:05:35 -0700

    Add Parks controller with constructor for instance of ParksApiContext database and 'get' and 'post' routes for the Api.

---
+++ ParksApi/Controllers/ParksController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParksApi.Models;
+
+namespace ParksApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private readonly ParksApiContext _db;
+
+    public ParksController(ParksApiContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/Parks
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get()
+    {
+      return await _db.Parks.ToListAsync();
+    }
+
+    // POST api/Parks
+    [HttpPost]
+    public async Task<ActionResult<Park>> Post(Park park)
+    {
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction("Post", new { id = park.ParkId }, park);
+    }
+  }
+}
